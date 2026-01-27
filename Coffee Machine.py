984bda9a40f481eafbc4a05c227b12283eed1bec
Monisss <monishedu@gmail.com>
2026-01-27 21:29:21 +0530

    Starting Out

---
+++ Coffee Machine.py
@@ -0,0 +1,21 @@
+
+coffee_menu = {
+    "espresso": {
+        "water": 50,
+        "milk": 0,
+        "coffee": 18
+    },
+    "latte": {
+        "water": 200,
+        "milk": 150,
+        "coffee": 24
+    },
+    "cappuccino": {
+        "water": 250,
+        "milk": 100,
+        "coffee": 24
+    }
+}
+
+Milk=coffee_menu["espresso"]["water"]
+print(Milk)
