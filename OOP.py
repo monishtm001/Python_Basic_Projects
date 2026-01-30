b186611753cc6ae003e885107924133a53c93217
Monisss <monishedu@gmail.com>
2026-01-30 21:24:00 +0530

    Using attributes of a class

--- OOP.py
+++ OOP.py
@@ -1,4 +1,19 @@
-from turtle import Turtle
-spinny=Turtle()
-spinny.shape("turtle")
-print(Turtle)
+# import turtle
+
+# screen = turtle.Screen()
+# screen.title("Turtle Test")
+
+# spinny = turtle.Turtle()
+# spinny.shape("turtle")
+
+# spinny.forward(100)
+
+# turtle.done()
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.field_names = ["Name", "Breakfast", "Dinner", "Night Wash"]
+table.add_column("Name", ["Adelaide", "Brisbane", "Darwin", "Hobart", "Sydney", "Melbourne", "Perth"])
+print(table)
+table.align = "r"
+print(table)
